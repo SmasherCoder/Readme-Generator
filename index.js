c0e84f97d3ade06b40c15f63a5338f240e5f0416
Travis Helms <smasher.coder@gmail.com>
2021-11-12 23:39:15 -0600

    added to markdown section

--- index.js
+++ index.js
@@ -1,13 +1,117 @@
 // TODO: Include packages needed for this application
+const fs = require('fs'); 
+const inquirer = require('inquirer'); 
+
+// linking to page where the README is developed 
+const generatePage = require('./utils/generateMarkdown.js');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'title',
+            message: 'What is your project name?',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your project name!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'list',
+            name: 'license',
+            message: 'What kind of license should your project have?',
+            choices: ['MIT', 'GNU', 'BSD', 'None'],
+        },
+        {
+            type: 'input',
+            name: 'credits',
+            message: 'Who has contributed to the project?',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please say who contributed to the project!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Please write a short description of your project.',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter a description of your project!');
+                    return false; 
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'install',
+            message: 'What are the steps required to install your project?',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter steps required to install your project!');
+                    return false; 
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'How do you use this app?',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter a usage description!');
+                    return false; 
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'contributors',
+            message: 'What does the user need to know about contributing to the repo?'
+        }
+    ])
+}
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+const writeFile = data => {
+    fs.writeFile('README.md', data, err => {
+        // if there is an error 
+        if (err) {
+            console.log(err);
+            return;
+        // when the README has been created 
+        } else {
+            console.log("Your README has been successfully created!")
+        }
+    })
+}; 
 
-// TODO: Create a function to initialize app
-function init() {}
-
-// Function call to initialize app
-init();
+// function call to initialize program
+questions()
+// getting user answers 
+.then(answers => {
+    return generatePage(answers);
+})
+// using data to display on page 
+.then(data => {
+    return writeFile(data);
+})
+// catching errors 
+.catch(err => {
+    console.log(err)
+})
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,18 +1,67 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (data.license === None){
+    return ``
+  } else {return `# ${data.title}
+  ![Github license](http://img.shields.io/badge/license-${data.license}-blue.svg)`
+  }
+
+Year = new Date().getFullYear
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (data.license === BSD) {
+    return `Information about the BSD license. https://choosealicense.com/licenses/bsd-2-clause/# `
+  } else if (data.license === GNU) {
+    return `Information about the GNU license. https://choosealicense.com/licenses/agpl-3.0/`
+  } else if (data.license === MIT) {
+    return `Information about the MIT license. https://choosealicense.com/licenses/mit/`
+  } else {
+    return ``
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+  ${renderLicenseBadge(license)}
+
+  ## Description
+  ${data.description}
+
+  ## Table of Contents
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [License](#license)
+  * [Contributing](#contributing)
+  * [Tests](#tests)
+  * [Questions](#questions)
+  
+  ## Installation 
+  ${data.install}
+
+  ## Usage 
+  ${data.usage}
+
+  ## License 
+  This project is license under ${data.license}
+
+  ## Contributing 
+  ${data.contributors}
+
+  ## Tests
+  ${data.test}
+
+  ## Questions
+  If you have any questions about this projects, please contact me directly at ${data.email}. You can view more of my projects at https://github.com/${data.github}.
 
 `;
 }
